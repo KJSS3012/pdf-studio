1d79513054b26d557b4f7e89775d4bd9cff10e29
Kaique Santos <kaique30122004@gmail.com>
2025-05-21 16:40:45 -0300

    feat: initialize Vue 3 project with Vite and Electron

---
+++ electron/main.js
@@ -0,0 +1,29 @@
+import { app, BrowserWindow } from 'electron'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  })
+
+  if (process.env.APP_MODE === 'development') {
+    win.loadURL('http://localhost:5173')
+  } else {
+    win.loadFile(path.join(__dirname, '../dist/index.html'))
+  }
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => app.quit())
